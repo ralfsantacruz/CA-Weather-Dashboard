7d2ffe34595654446a8d78ae6a4caf7f91f1fffa
ralfsantacruz <rsantac1@gmail.com>
2018-11-26 20:02:30 -0800

    initial code upload

---
+++ open_weather/config.py
@@ -0,0 +1,4 @@
+owm_api_key = "&APPID=627fe73e331005c5c18a30f6005cf3f7"
+b_api_key = "0018f6dd380248939d01e548b67673f1"
+g_api_key = "AIzaSyAZYxv0wjHKxZ1ZsInapq5mOkp8VUUH7pQ"
+py_api_key = "m43Oz35O5mKYCxDDJoDq"
