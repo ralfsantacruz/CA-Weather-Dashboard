b1a37b4d3b95b9846cab0f8c5073e0a2e0b8ab92
ralfsantacruz <rsantac1@gmail.com>
2018-12-05 21:29:28 -0800

    made landing page look pretty

--- open_weather/app.py
+++ open_weather/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, Markup
 from graph_gen import generate_plot
-from map_gen import generate_scattermap
+from map_gen import generate_scattermap, utc_to_pst
+
+from datetime import datetime
+from pytz import timezone
+import pytz
 
 from sqlalchemy import create_engine
 
@@ -8,25 +12,30 @@ import pandas as pd
 
 # Create connection to SQL
 engine = create_engine("mysql://root:password@localhost/weather_data")
-query='''select * from california_weather'''
+query='''select * from california_weather where day(date) = 5 and hour(date) between 00 and 01'''
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
 def home():
-    
+
     if request.method == 'GET':
-        # df = pd.read_sql_query(query, engine).set_index('index')
-        df = pd.read_csv('test_data2.csv')
+        df = pd.read_sql_query(query, engine)
+        # df = pd.read_csv('test_data2.csv')
+        pst_time = utc_to_pst(df['date'][0])
         map_html = Markup(generate_scattermap(df))
         # plot_html = generate_plot("san francisco")
     else:
-        # text = request.form['text']
-        # plot_html = generate_plot(text)
-        df = pd.read_sql_query(query, engine)
-        map_html = generate_scattermap(df)
+        # Get form data as SQL query
+        text = request.form['text']
+        # Plug into pandas to return data for query.
+        df = pd.read_sql_query(text, engine)
 
-    return render_template("index.html",map_html=map_html)
+        pst_time = utc_to_pst(df['date'][0])
+        map_html = Markup(generate_scattermap(df))
+        # plot_html = generate_plot(text)
+
+    return render_template("index.html",map_html=map_html,date_shown=pst_time)
 
 # @app.route('/form', methods=['POST'])
 # def my_form_post():
--- open_weather/map_gen.py
+++ open_weather/map_gen.py
@@ -13,9 +13,62 @@ from config import mapbox_api_key
 import pandas as pd
 import numpy as np
 
+from pytz import timezone
+import pytz
+
+from datetime import datetime as dt
+
 # # Mapping functions:
 
 # In[36]:
+def utc_to_pst(date):
+    d = pytz.utc.localize(date)
+    pst = d.astimezone(timezone('US/Pacific'))
+    parsed_pst = dt.strftime(pst, '%Y-%m-%d %I:%M %p')
+    
+    return parsed_pst
+
+# def check_col(df,col):
+    
+#     '''Checks column and returns appropriate parameters for mapping.'''
+    
+#     size = df[col]
+#     colorscale = 'Jet'
+#     reversescale=False
+    
+#     if col == 'rain':
+#         string = "{}<br>Value: {}mm"
+#         size = df[col]*3
+#         colorscale = 'Blues'
+#         reversescale=True
+        
+#     elif col == 'wind_speed':
+#         string = "{}<br>Value: {}m/s"
+#         size = df[col]*3
+        
+#     elif col == 'temperature':
+#         string = "{}<br>Value: {}\u00b0F"
+#         size = df[col]/2.5
+        
+#     elif col == 'cloud':
+#         string = "{}<br>Percentage: {}%"
+#         size = df[col]/2
+#         colorscale = 'Blues'
+        
+#     elif col == 'pressure':
+#         string = "{}<br>Value: {}hPa"
+#         size = df[col]/100
+#         colorscale='Viridis'
+        
+#     elif col == 'aqi':
+#         string = "{}<br>Value: {}<br>{}<br>Dominant pollutant: {}"
+#         size = df[col]/10
+        
+#     else:
+#         string = "{}<br>Value: {}mW/m\u00b2"
+#         size = df[col]*2
+    
+#     return size,string,colorscale,reversescale
 
 def check_col(df,col):
     
@@ -26,35 +79,35 @@ def check_col(df,col):
     reversescale=False
     
     if col == 'rain':
-        string = "{}<br>Value: {}mm"
-        size = df[col]*3
+        string = "{}<br>Value: {}mm<br>Date: {}"
+        size = df[col]*2
         colorscale = 'Blues'
         reversescale=True
         
     elif col == 'wind_speed':
-        string = "{}<br>Value: {}m/s"
-        size = df[col]*3
+        string = "{}<br>Value: {}m/s<br>Date: {}"
+        size = df[col]*1.5
         
     elif col == 'temperature':
-        string = "{}<br>Value: {}\u00b0F"
-        size = df[col]/2.5
+        string = "{}<br>Value: {}\u00b0F<br>Date: {}"
+        size = df[col]/3
         
     elif col == 'cloud':
-        string = "{}<br>Percentage: {}%"
-        size = df[col]/2
+        string = "{}<br>Percentage: {}%<br>Date: {}"
+        size = df[col]/1.5
         colorscale = 'Blues'
         
     elif col == 'pressure':
-        string = "{}<br>Value: {}hPa"
+        string = "{}<br>Value: {}hPa<br>Date: {}"
         size = df[col]/100
-        colorscale='Viridis'
+        colorscale='YlGnBu'
         
     elif col == 'aqi':
-        string = "{}<br>Value: {}<br>{}<br>Dominant pollutant: {}"
+        string = "{}<br>Value: {}<br>{}<br>Dominant pollutant: {}<br>Date: {}"
         size = df[col]/10
         
     else:
-        string = "{}<br>Value: {}mW/m\u00b2"
+        string = "{}<br>Value: {}mW/m\u00b2<br>Date: {}"
         size = df[col]*2
     
     return size,string,colorscale,reversescale
@@ -62,6 +115,26 @@ def check_col(df,col):
 
 # In[8]:
 
+
+# def marker_text(df,col,fstring):
+#     '''
+#     Generates hover-over text for weather info.
+#     '''
+#     text = []
+#     for row in df.itertuples():
+#         if col == 'aqi':
+#             a = getattr(row, "city")
+#             b = getattr(row, col)
+#             c = getattr(row, "category")
+#             d = getattr(row, "dominant_pollutant")
+#             marker_text = fstring.format(a,b,c,d)
+#             text.append(marker_text)
+#         else:
+#             a = getattr(row, "city")
+#             b = getattr(row, col)
+#             marker_text = fstring.format(a,b)
+#             text.append(marker_text)
+#     return text
 
 def marker_text(df,col,fstring):
     '''
@@ -74,12 +147,14 @@ def marker_text(df,col,fstring):
             b = getattr(row, col)
             c = getattr(row, "category")
             d = getattr(row, "dominant_pollutant")
-            marker_text = fstring.format(a,b,c,d)
+            e = utc_to_pst(getattr(row, "date"))
+            marker_text = fstring.format(a,b,c,d,e)
             text.append(marker_text)
         else:
             a = getattr(row, "city")
             b = getattr(row, col)
-            marker_text = fstring.format(a,b)
+            c = utc_to_pst(getattr(row, "date"))
+            marker_text = fstring.format(a,b,c)
             text.append(marker_text)
     return text
 
